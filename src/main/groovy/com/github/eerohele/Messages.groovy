e38a1b15c205b886b14a3f3427dac4ff2ec89afb
Eero Helenius <eero.helenius@kapsi.fi>
2019-02-25 16:25:06 +0200

    Delete unused message

--- src/main/groovy/com/github/eerohele/Messages.groovy
+++ src/main/groovy/com/github/eerohele/Messages.groovy
@@ -1,6 +1,4 @@
 messages {
-    javaVersionWarning = '''There\'s a memory leak in Gradle that causes a PermGen error after about 20 runs with the same Gradle daemon.
-You might want to consider using Java 8 instead.'''
     classpathError = '''Could not set up the classpath. Does the ditaOt property point to a working DITA-OT
 installation? If yes, please report this error in the GitHub issue tracker.'''
     ditaHomeError = '''DITA Open Toolkit directory not set.
