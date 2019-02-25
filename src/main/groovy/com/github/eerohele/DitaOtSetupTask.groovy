d02d25ce2aa9786cfe1aee8addcaf7a9ae53abb4
Eero Helenius <eero.helenius@kapsi.fi>
2019-02-25 15:57:37 +0200

    Deprecate ditaOt task #19

--- src/main/groovy/com/github/eerohele/DitaOtSetupTask.groovy
+++ src/main/groovy/com/github/eerohele/DitaOtSetupTask.groovy
@@ -6,44 +6,61 @@ import org.gradle.api.file.FileCollection
 import org.gradle.api.file.FileTree
 import org.gradle.api.tasks.TaskAction
 
+@Deprecated
 class DitaOtSetupTask extends DefaultTask {
     FileCollection classpath
     File dir
     List<Object> plugins
 
+    @Deprecated
     @SuppressWarnings('ConfusingMethodName')
     void plugins(Object... plugins) {
+        logger.warn('The "ditaOt" task is deprecated.')
         this.plugins = plugins
     }
 
+    @Deprecated
     @SuppressWarnings('ConfusingMethodName')
     void dir(Object dir) {
+        logger.warn('The "ditaOt" task is deprecated. Use dita { ... ditaOt "/path/to/dita-ot" ... } instead.')
         Options.ditaOt = project.ditaOt.dir = project.file(dir)
     }
 
+    @Deprecated
     @SuppressWarnings('ConfusingMethodName')
     void classpath(Object... classpath) {
+        logger.warn('The "ditaOt" task is deprecated.')
         this.classpath = project.files(classpath)
     }
 
+    @Deprecated
     FileTree getDefaultClasspath(File ditaHome) {
+        logger.warn('The "ditaOt" task is deprecated.')
         Classpath.compile(ditaHome).getAsFileTree()
     }
 
+    @Deprecated
     FileTree getDefaultClasspath(Project project) {
+        logger.warn('The "ditaOt" task is deprecated.')
         Classpath.compile(project, project.ditaOt.dir).getAsFileTree()
     }
 
+    @Deprecated
     FileTree getPluginClasspath(File ditaHome) {
+        logger.warn('The "ditaOt" task is deprecated.')
         Classpath.pluginClasspath(ditaHome).getAsFileTree()
     }
 
+    @Deprecated
     FileTree getPluginClasspath(Project project) {
+        logger.warn('The "ditaOt" task is deprecated.')
         Classpath.pluginClasspath(project.ditaOt.dir).getAsFileTree()
     }
 
     @TaskAction
+    @Deprecated
     void install() {
+        logger.warn('The "ditaOt" task is deprecated.')
         if (this.plugins != null && this.dir != null) {
             AntBuilderAssistant.getAntBuilder(getDefaultClasspath(this.dir)).execute {
                 this.plugins.each { Object plugin ->
